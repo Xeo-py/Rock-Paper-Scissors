033aee69e795caa1cdc64d5fd3f45b4328fda88e
Xeo-py <sahilkt505@gmail.com>
2026-02-21 15:21:35 +0000

    Added UI

--- js/javascript.js
+++ js/javascript.js
@@ -25,54 +25,61 @@ function playRound()
     const humanChoice=HumAns.toLowerCase();
     const computerChoice=ComAns;
     if(humanChoice==="rock" && computerChoice==="paper"){
-        console.log("You lose! Paper bests Rock.");
+        display.textContent="You lose! Paper bests Rock.";
         computerScore++;
     }
     else if(humanChoice==="rock" && computerChoice==="scissors"){
-        console.log("You won! Rock bests Scissors.");
+        display.textContent="You won! Rock bests Scissors.";
         humanScore++;
     }
     else if(humanChoice==="rock" && computerChoice==="rock"){
-        console.log("Draw! same choice.");
+        display.textContent="Draw! same choice.";
     }
     else if(humanChoice==="paper" && computerChoice==="scissors"){
-        console.log("You lose! Scissors bests paper.");
+        display.textContent="You lose! Scissors bests paper.";
         computerScore++;
     }
     else if(humanChoice==="paper" && computerChoice==="rock"){
-        console.log("You won! Paper bests rock.");
+        display.textContent="You won! Paper bests rock.";
         humanScore++;
     }
     else if(humanChoice==="paper" && computerChoice==="paper"){
-        console.log("Draw! same choice.");
+        display.textContent="Draw! same choice.";
     }
     else if(humanChoice==="scissors" && computerChoice==="paper"){
-        console.log("You won! Scissors bests paper.");
+        display.textContent="You won! Scissors bests paper.";
         humanScore++;
     }
     else if(humanChoice==="scissors" && computerChoice==="rock"){
-        console.log("You lose! Rock bests Scissors.");
+        display.textContent="You lose! Rock bests Scissors.";
         computerScore++;
     }
     else if(humanChoice==="scissors" && computerChoice==="scissors"){
-        console.log("Draw! same choice.");
+        display.textContent="Draw! same choice.";
     }
+    score.textContent=`Your Score=${humanScore}, Computer Score=${computerScore}`
 }
-function playGame()
-{
-    for(let i=1;i<=5;i++){
-        GetHumanChoice();
-        GetComputerChoice();
-        playRound();
-    }
-    if(humanScore>computerScore){
-        console.log(`You Won!\nYoue Score=${humanScore}\nComputer Score=${computerScore}`);
-    }
-    else if(humanScore<computerScore){
-        console.log(`You Lose!\nYoue Score=${humanScore}\nComputer Score=${computerScore}`);
-    }
-    if(humanScore===computerScore){
-        console.log(`Draw!\nYoue Score=${humanScore}\nComputer Score=${computerScore}`);
-    }
-}
-playGame();
+// function playGame()
+// {
+//     for(let i=1;i<=5;i++){
+//         GetHumanChoice();
+//         GetComputerChoice();
+//         playRound();
+//     }
+//     if(humanScore>computerScore){
+//         console.log(`You Won!\nYoue Score=${humanScore}\nComputer Score=${computerScore}`);
+//     }
+//     else if(humanScore<computerScore){
+//         console.log(`You Lose!\nYoue Score=${humanScore}\nComputer Score=${computerScore}`);
+//     }
+//     if(humanScore===computerScore){
+//         console.log(`Draw!\nYoue Score=${humanScore}\nComputer Score=${computerScore}`);
+//     }
+// 
+const body=document.querySelector("body");
+const btns = document.querySelectorAll("button");
+btns.forEach(btn => { btn.addEventListener("click", playRound); });
+const display=document.createElement("div");
+const score=document.createElement("p");
+body.appendChild(display);
+body.appendChild(score);
